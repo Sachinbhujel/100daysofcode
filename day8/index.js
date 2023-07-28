186fc8283555bea892c359462fed89dc66518f5a
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2023-07-28 23:19:44 +0530

    Create index.js

---
+++ day8/index.js
@@ -0,0 +1,9 @@
+function submit() {
+  document.getElementById("para2").style.display = "block";
+  document.getElementById("para").style.display = "none";
+}
+
+function done() {
+  document.getElementById("para2").style.display = "none";
+  document.getElementById("para").style.display = "block";
+}
