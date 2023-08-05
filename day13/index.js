a33176b04eb4b5ea22e091d7f3820e96b97eb730
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2023-08-05 22:38:24 +0530

    Update index.js

--- day13/index.js
+++ day13/index.js
@@ -1,1 +1,22 @@
+var slideIndex = 1;
+showSlide(slideIndex);
 
+function plusOrMinus(n){
+  showSlide(slideIndex += n);
+}
+
+function showSlide(n){
+  var photos = document.getElementsByClassName("photos");
+  
+  if (n > photos.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = photos.length;
+  }
+  
+  for (i = 0; i < photos.length; i++) {
+    photos[i].style.display = "none"; 
+  }
+  photos[slideIndex-1].style.display = "block";
+}
