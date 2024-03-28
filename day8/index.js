7d532a9e6105a54a313f283aa3919b18c827ebf0
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2024-03-28 23:15:25 +0530

    Delete day8/index.js

--- day8/index.js
+++
@@ -1,9 +0,0 @@
-function submit() {
-  document.getElementById("para2").style.display = "block";
-  document.getElementById("para").style.display = "none";
-}
-
-function done() {
-  document.getElementById("para2").style.display = "none";
-  document.getElementById("para").style.display = "block";
-}
